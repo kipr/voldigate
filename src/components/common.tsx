5727faf866db75178ba73a93003a2805ca442a78
Erin <erinharrington12@yahoo.com>
2024-09-09 10:30:27 -0500

    Added different components for EditorBar dependent if FileExplorer is opened or not

--- src/components/common.tsx
+++ src/components/common.tsx
@@ -3,8 +3,11 @@ import { styled } from 'styletron-react';
 export const Spacer = styled('div', {
   flex: '1 1'
 });
-
-export const leftBarSpacer = styled('div', {
+export const leftBarSpacerOpen = styled('div', {
+  //marginLeft: '250px'
+  flex: '1.5 1'
+});
+export const leftBarSpacerClosed = styled('div', {
   //marginLeft: '250px'
   flex: '5 1'
 });
@@ -15,7 +18,12 @@ export const middleBarSpacer = styled('div', {
   
 });
 
-export const rightBarSpacer = styled('div', {
+export const rightBarSpacerOpen = styled('div', {
+  // marginRight: '210px'
+  flex: '1.25 1'
+});
+
+export const rightBarSpacerClosed= styled('div', {
   // marginRight: '210px'
   flex: '5 1'
 });
