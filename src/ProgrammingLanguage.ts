11e8097506d2cc270baf75171ee244dae5b0fcf8
Erin <erinharrington12@yahoo.com>
2024-09-09 10:29:39 -0500

    Added default code for header and user data files. Also added plaintext as a programming language for Monaco editor to recognize and color map words

--- src/ProgrammingLanguage.ts
+++ src/ProgrammingLanguage.ts
@@ -1,18 +1,25 @@
-type ProgrammingLanguage = 'c' | 'cpp' | 'python';
+type ProgrammingLanguage = 'c' | 'cpp' | 'python' | 'plaintext';
 
 namespace ProgrammingLanguage {
   export const FILE_EXTENSION: { [key in ProgrammingLanguage]: string } = {
     c: 'c',
     cpp: 'cpp',
-    python: 'py'
+    python: 'py',
+    plaintext: 'txt',
   };
 
 
   export const DEFAULT_CODE: { [key in ProgrammingLanguage]: string } = {
     c: '#include <stdio.h>\n#include <kipr/wombat.h>\n\nint main()\n{\n  printf("Hello, World!\\n");\n  return 0;\n}\n',
     cpp: '#include <iostream>\n#include <kipr/wombat.hpp>\n\nint main()\n{\n  std::cout << "Hello, World!" << std::endl;\n  return 0;\n}\n',
-    python: 'from kipr import *\n\nprint(\'Hello, World!\')'
+    python: 'from kipr import *\n\nprint(\'Hello, World!\')',
+    plaintext: '*Your User Data Here*',
   };
+
+
+  export const DEFAULT_HEADER_CODE = '#include <kipr/wombat.h>\n';
+
+  export const DEFAULT_USER_DATA_CODE = '*Your User Data Here*';
 }
 
 export default ProgrammingLanguage;
