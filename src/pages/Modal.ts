ba294eb34cc82f41f913e69d5d50156856348c06
Erin <erinharrington12@yahoo.com>
2024-09-10 14:45:19 -0500

    Adding Modal for includes

---
+++ src/pages/Modal.ts
@@ -0,0 +1,102 @@
+import construct from '../util/construct';
+
+
+export namespace Modal {
+  export enum Type {
+    Settings,
+    About,
+    Exception,
+    OpenScene,
+    Feedback,
+    FeedbackSuccess,
+    None,
+    NewScene,
+    CopyScene,
+    SettingsScene,
+    DeleteRecord,
+    ResetCode,
+    CreateFile,
+    CreateUser,
+    CreateProject,
+    OpenUsers
+  }
+  
+  export interface Settings {
+    type: Type.Settings;
+  }
+  
+  export const SETTINGS: Settings = { type: Type.Settings };
+  
+  export interface About {
+    type: Type.About;
+  }
+  
+  export const ABOUT: About = { type: Type.About };
+  
+  export interface Feedback {
+    type: Type.Feedback;
+  }
+  
+  export const FEEDBACK: Feedback = { type: Type.Feedback };
+  
+  export interface FeedbackSuccess {
+    type: Type.FeedbackSuccess;
+  }
+  
+  export const FEEDBACKSUCCESS: FeedbackSuccess = { type: Type.FeedbackSuccess };
+  
+  export interface Exception {
+    type: Type.Exception;
+    error: Error;
+    info?: React.ErrorInfo;
+  }
+  
+  export const exception = (error: Error, info?: React.ErrorInfo): Exception => ({ type: Type.Exception, error, info });
+
+  
+  export interface None {
+    type: Type.None;
+  }
+  
+  export const NONE: None = { type: Type.None };
+  
+  export interface CreateProject {
+    type: Type.CreateProject;
+  }
+
+  export const CREATEPROJECT: CreateProject = { type: Type.CreateProject };
+
+
+  export interface CreateFile {
+    type: Type.CreateFile;
+  }
+
+  export const CREATEFILE: CreateFile = { type: Type.CreateFile };
+
+  export interface CreateUser {
+    type: Type.CreateUser;
+  }
+
+  export const CREATEUSER: CreateUser = { type: Type.CreateUser };
+
+  export interface OpenUsers {
+    type: Type.OpenUsers;
+  }
+
+  export const OPENUSERS: OpenUsers = { type: Type.OpenUsers };
+
+}
+  
+export type Modal = (
+  Modal.Settings |
+  Modal.About |
+  Modal.Exception |
+  Modal.Feedback |
+  Modal.FeedbackSuccess |
+  Modal.None |
+
+  Modal.CreateProject |
+  Modal.CreateFile |
+  Modal.CreateUser |
+  Modal.OpenUsers
+);
