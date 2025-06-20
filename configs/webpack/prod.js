c728fa765777d61adcb0858328d13e8fe57639ec
Erin <erinharrington12@yahoo.com>
2025-06-20 11:19:03 -0500

    Added webp image processing

--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -1,8 +1,15 @@
 // production config
 const { merge } = require('webpack-merge');
-
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 const commonConfig = require('./common');
 
 module.exports = merge(commonConfig, {
   mode: 'production',
+  plugins: [
+    new MonacoWebpackPlugin({
+      // Only include the languages/features you actually use
+      languages: ['javascript', 'python', 'typescript'],
+      features: ['coreCommands', 'find', 'bracketMatching', 'comment', 'folding'],
+    }),
+  ],
 });
