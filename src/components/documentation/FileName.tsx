ae63ebea0da8c2b6a07e1624e03a715443bdf4d4
Erin <erinharrington12@yahoo.com>
2025-06-24 14:14:48 -0500

    Removed voldigate specific documentation,  now depends on shared Ivygate repo

--- src/components/documentation/FileName.tsx
+++
@@ -1,8 +0,0 @@
-import { styled } from 'styletron-react';
-
-const FileName = styled('span', ({ onClick }: { onClick?: (event: React.MouseEvent) => void }) => ({
-  fontFamily: 'monospace',
-  cursor: onClick ? 'pointer' : 'default',
-}));
-
-export default FileName;
