ae31749a445cc31601ff138e246992c522d749a8
Erin <erinharrington12@yahoo.com>
2025-09-23 16:36:05 -0500

    Changed tab indentation from two spaces to one

--- src/ProgrammingLanguage.js
+++ src/ProgrammingLanguage.js
@@ -9,8 +9,8 @@ var ProgrammingLanguage;
         plaintext: 'txt'
     };
     ProgrammingLanguage.DEFAULT_CODE = {
-        c: '#include <stdio.h>\n#include <kipr/wombat.h>\n\nint main()\n{\n  printf("Hello, World!\\n");\n  return 0;\n}\n',
-        cpp: '#include <iostream>\n#include <kipr/wombat.hpp>\n\nint main()\n{\n  std::cout << "Hello, World!" << std::endl;\n  return 0;\n}\n',
+        c: '#include <stdio.h>\n#include <kipr/wombat.h>\n\nint main()\n{\n\tprintf("Hello, World!\\n");\n\treturn 0;\n}\n',
+        cpp: '#include <iostream>\n#include <kipr/wombat.hpp>\n\nint main()\n{\n\tstd::cout << "Hello, World!" << std::endl;\n\treturn 0;\n}\n',
         python: 'from kipr import *\n\nprint(\'Hello, World!\')',
         plaintext: '*Your User Data Here*'
     };
@@ -18,3 +18,4 @@ var ProgrammingLanguage;
     ProgrammingLanguage.DEFAULT_USER_DATA_CODE = '*Your User Data Here*';
 })(ProgrammingLanguage || (ProgrammingLanguage = {}));
 exports["default"] = ProgrammingLanguage;
+//
