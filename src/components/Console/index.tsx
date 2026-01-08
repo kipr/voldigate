58cc2f91dae4431f5afca81a84135c3b29d9e1db
Erin <erinharrington12@yahoo.com>
2026-01-08 15:49:56 -0600

    Change Compile bar's background color to match compiled state (succeeded, warning, failed)

--- src/components/Console/index.tsx
+++ src/components/Console/index.tsx
@@ -4,35 +4,41 @@ import { StyleProps } from '../../style';
 import { StyledText } from '../../util';
 import ScrollArea from '../ScrollArea';
 import { Text } from '../Text';
-import { Theme, ThemeProps } from '../theme';
+import { RED, Theme, ThemeProps } from '../theme';
 import { Fa } from '../Fa';
 import { Button } from '../Button';
 import { BarComponent } from '../Widget';
 import { faFile } from '@fortawesome/free-solid-svg-icons';
 import LocalizedString from '../../util/LocalizedString';
 import tr from '@i18n';
-
 export const createConsoleBarComponents = (
-  theme: Theme, 
+  theme: Theme,
   onClearConsole: () => void,
-  locale: LocalizedString.Language
+  locale: LocalizedString.Language,
+  compileStatus?: 'idle' | 'compiling' | 'success' | 'warning' | 'error',
 ) => {
-  // eslint-disable-next-line @typescript-eslint/ban-types
   const consoleBar: BarComponent<object>[] = [];
 
-  consoleBar.push(BarComponent.create(Button, {
-    theme,
-    onClick: onClearConsole,
-    children:
-      <>
-        <Fa icon={faFile} />
-        {' '} {LocalizedString.lookup(tr('Clear'), locale)}
-      </>,
-  }));
-
+  console.log("createConsoleBarComponents compileStatus:", compileStatus);
+  consoleBar.push(
+    BarComponent.create(
+      Button,
+      {
+        theme,
+        onClick: onClearConsole,
+        children: (
+          <>
+            <Fa icon={faFile} />{' '}
+            {LocalizedString.lookup(tr('Clear'), locale)}
+          </>
+        ),
+      },
+      { chromeBackgroundColor: compileStatus === 'idle' ? `rgba(0, 0, 0, 0.1)` : compileStatus === 'compiling' ?  `rgba(0, 0, 0, 0.1)`: compileStatus === 'success' ? theme.compileSuccessColor : compileStatus === 'warning' ? theme.compileWarningColor : theme.compileFailedColor } // Green for success, yellow for warning, red for error
+    )
+  );
+//
   return consoleBar;
 };
-
 export interface ConsoleProps extends StyleProps, ThemeProps {
   text: StyledText;
 }
